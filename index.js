4eb95ddca1d55a034a28587a22c22b8338364043
Omar <otantawy53@gmail.com>
2022-05-02 23:35:16 -0400

    commit tree 3aa47dafde39322cd63f061e373c01af95cd5269

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+let text = document.querySelector('#text');
+
+document.addEventListener('DOMContentLoaded' , function(){
+    // console.log('is this working');
+    text.textContent = 'This is really cool!';
+})
+
+// console.log(text.textContent)
